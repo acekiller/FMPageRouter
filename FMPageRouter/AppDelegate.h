4b013ae7e96f04f0efbf26ee2e893c8a31181ab6
acekiller <fengxijun51020@hotmail.com>
2017-04-28 15:54:36 +0800

    init FMPageRouter

---
+++ FMPageRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMPageRouter
+//
+//  Created by Fantasy on 2017/4/28.
+//  Copyright © 2017年 fantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMPageRouter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMPageRouter
+//
+//  Created by Fantasy on 2017/4/28.
+//  Copyright © 2017年 fantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
