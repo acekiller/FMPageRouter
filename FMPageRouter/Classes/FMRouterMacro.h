6087bcb63e1babca68002f266f53d41381d12a27
acekiller <fengxijun51020@hotmail.com>
2017-05-12 09:25:25 +0800

    add macro define file

---
+++ FMPageRouter/Classes/FMRouterMacro.h
@@ -0,0 +1,14 @@
+//
+//  FMRouterMacro.h
+//  FMPageRouter
+//
+//  Created by Fantasy on 2017/5/12.
+//  Copyright © 2017年 fantasy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define FMPageRouterURLNoStaticNode 10000
+#define FMPageRouterURLConflict 10001
+#define FMPageRouterURLIllegal 10002
+#define FMPageRouterURLMatchPagFailed 10003
